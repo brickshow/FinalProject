72dde4bbea667343fc7fb6442ed1ad952fcf50cd
brickshow <127967015+brickshow@users.noreply.github.com>
2023-10-26 11:09:01 +0800

    Add files via upload

---
+++ INVENTORYSYSTEM/INVENTORYSYSTEM/titlePage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVENTORYSYSTEM
+{
+    public class titlePage
+    {
+        public static void theTitle()
+        {
+
+            var title = Console.ReadLine();
+            
+        }
+    }
+}
